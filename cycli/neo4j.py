94fcb943ab9b156fed4c7560fe0d8b4ab7e4c6db
Nicole White <nicole.m.white@outlook.com>
2015-08-19 15:23:56 -0700

    Merge didn't grab the ssl arg to Neo4j
    Closes #12.

--- cycli/neo4j.py
+++ cycli/neo4j.py
@@ -4,9 +4,7 @@ from py2neo import Graph, authenticate
 
 class Neo4j:
 
-    def __init__(self, host, port, username=None, password=None):
-        self.host = host
-        self.port = port
+    def __init__(self, host, port, username=None, password=None, ssl=False):
         self.username = username
         self.password = password
 
