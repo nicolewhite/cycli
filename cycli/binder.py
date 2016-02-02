29252aa8ec104328e620aa25b2c3e9f5e40d074e
Nicole White <nicole.m.white@outlook.com>
2016-02-02 10:19:05 -0800

    Add backspace binding :back:
    If the user types () and then backspaces while their cursor
    is between the ( and ), then delete both the ( and ). This
    applies for (), [], {}, '', "", and ``.

--- cycli/binder.py
+++ cycli/binder.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 from prompt_toolkit.key_binding.manager import KeyBindingManager
+from prompt_toolkit.keys import Keys
 
 CypherBinder = KeyBindingManager()
 
@@ -83,3 +84,19 @@ def backtick(event):
     else:
         b.insert_text("`")
         b.insert_text("`", move_cursor=False)
+
+@CypherBinder.registry.add_binding(Keys.Backspace)
+def backspace(event):
+    b = event.cli.current_buffer
+    current_char = b.document.current_char
+    before_char = b.document.char_before_cursor
+
+    patterns = [("(", ")"), ("[", "]"), ("{", "}"), ("'", "'"), ('"', '"'), ("`", "`")]
+
+    for pattern in patterns:
+        if before_char == pattern[0] and current_char == pattern[1]:
+            b.cursor_right()
+            b.delete_before_cursor(2)
+            return
+
+    b.delete_before_cursor()
--- tests/test_binder.py
+++ tests/test_binder.py
@@ -1,14 +1,13 @@
 from __future__ import unicode_literals
-from cycli.binder import curly_left, curly_right, paren_left, paren_right, bracket_left, bracket_right, apostrophe, \
-    quote, backtick
-from mock import call
 
+from mock import call, MagicMock
 import pytest
+
+from cycli.binder import *
 
 
 @pytest.fixture
 def binder():
-    from mock import MagicMock
     return MagicMock()
 
 
@@ -97,3 +96,29 @@ def test_backtick_not_current(binder):
     binder.cli.current_buffer.document.current_char = "n"
     backtick(binder)
     binder.cli.current_buffer.insert_text.assert_has_calls([call("`"), call("`", move_cursor=False)])
+
+
+def test_backspace_no_pattern(binder):
+    binder.cli.current_buffer.document.current_char = "n"
+    backspace(binder)
+    binder.cli.current_buffer.delete_before_cursor.assert_called_once_with()
+
+
+@pytest.mark.parametrize(("char_before_cursor", "current_char"), [
+    ("(", ")"),
+    ("[", "]"),
+    ("{", "}"),
+    ("'", "'"),
+    ('"', '"'),
+    ("`", "`")
+])
+def test_backspace_with_pattern(char_before_cursor, current_char):
+    binder = MagicMock()
+
+    binder.cli.current_buffer.document.char_before_cursor = char_before_cursor
+    binder.cli.current_buffer.document.current_char = current_char
+
+    backspace(binder)
+
+    binder.cli.current_buffer.cursor_right.assert_called_once_with()
+    binder.cli.current_buffer.delete_before_cursor.assert_called_once_with(2)
